2afa9b6649198bbfb3b0830957e529a441caf061
jaehong <jaehong.choi@morulabs.com>
2019-06-26 16:28:04 +0900

    update

--- index.js
+++ index.js
@@ -9,76 +9,78 @@ const API = {
 
 }
 
-module.exports = function(){
-    let endpoint;
-    let auth;
-    let contentType;
-    let headers;
-    
-    return {
-        setEndpoint: function(addr) {
-            endpoint = addr
-            return this
-        },
-        setAuth: function(token) {
-            auth = `JWT ${token}`
-            if(!headers) headers = {}
-            headers['Authorization'] = auth
-            return this
-        },
-        setContentType: function(type) {
-            contentType = type
-            if(!headers) headers = {}
-            headers['Content-Type'] = contentType
-            return this
-        },
-        /**
-         * login
-         * 
-         * @param {email,password} data 
-         */
-        login: function(data) {
-            return axios.post(
-                endpoint + API.LOGIN,
-                data,
-                { headers },
-            )
-        },
-        /**
-         * verify token
-         * 
-         * @param {token} data 
-         */
-        verifyToken: function(data) {
-            return axios.post(
-                endpoint + API.VERIFY_TOKEN,
-                data,
-                { headers }
-            )
-        },
-        /**
-         * verify staff
-         * 
-         * @param {country_code} data 
-         */
-        verifyStaff: function(data) {
-            return axios.post(
-                endpoint + API.VERIFY_TOKEN,
-                data,
-                { headers }
-            )
-        },
-        /**
-         * verify manager
-         * 
-         * @param {country_code} data 
-         */
-        verifyManager: function(data) {
-            return axios.post(
-                endpoint + API.VERIFY_MANAGER,
-                data,
-                { headers }
-            )
-        },
-    }
+let endpoint;
+let auth;
+let contentType;
+let headers;
+
+export default moruApi = {
+    setEndpoint: function (addr) {
+        endpoint = addr
+        return this
+    },
+    setAuth: function (token) {
+        auth = `JWT ${token}`
+        if (!headers) headers = {}
+        headers['Authorization'] = auth
+        return this
+    },
+    setContentType: function (type) {
+        contentType = type
+        if (!headers) headers = {}
+        headers['Content-Type'] = contentType
+        return this
+    },
+    /**
+     * login
+     * 
+     * @param {email,password} data 
+     */
+    login: function (data) {
+        return axios.post(
+            endpoint + API.LOGIN,
+            data, {
+                headers
+            },
+        )
+    },
+    /**
+     * verify token
+     * 
+     * @param {token} data 
+     */
+    verifyToken: function (data) {
+        return axios.post(
+            endpoint + API.VERIFY_TOKEN,
+            data, {
+                headers
+            }
+        )
+    },
+    /**
+     * verify staff
+     * 
+     * @param {country_code} data 
+     */
+    verifyStaff: function (data) {
+        return axios.post(
+            endpoint + API.VERIFY_TOKEN,
+            data, {
+                headers
+            }
+        )
+    },
+    /**
+     * verify manager
+     * 
+     * @param {country_code} data 
+     */
+    verifyManager: function (data) {
+        return axios.post(
+            endpoint + API.VERIFY_MANAGER,
+            data, {
+                headers
+            }
+        )
+    },
 }
